15e46b9ae886705504d914a7c894ca9f205a7182
Arsalan Ahmed <arskhatri2008@gmail.com>
2023-09-18 16:55:31 +0500

    continue

--- pinecone.mjs
+++ pinecone.mjs
@@ -1,6 +1,9 @@
 
 import { Pinecone } from "@pinecone-database/pinecone";
 import OpenAI from "openai";
+// import { HfInference } from '@huggingface/inference'
+
+
 
 const pinecone = new Pinecone({
     environment: process.env.PINECONE_ENVIRONMENT,
@@ -14,3 +17,4 @@ export const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+// export const hf = new HfInference(process.env.HUGGINGFACE_API_KEY)
